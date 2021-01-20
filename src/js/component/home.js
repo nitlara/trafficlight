ec489da357e6b2414c00bce7908477e8ad0a233f
nitlara <nitlara@gmail.com>
2021-01-20 17:16:28 +0000

    traffic light

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export function Trafficlight() {
+	//useState con light
+	const [light, setLight] = useState("");
+	//
 
-//create your first component
-export function Home() {
+	if (light == "red") {
+		var redLightSelection = "selected";
+	} else if (light == "orange") {
+		var orangeLightSelection = "selected";
+	} else {
+		var greenLightSelection = "selected";
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5 d-flex flex-column align-items-center justify-content-around">
+			<div
+				className={
+					"red circle rounded-circle border-dark " + redLightSelection
+				}
+				onClick={() => setLight("red")}
+			/>
+			<div
+				className={
+					"orange circle rounded-circle border-dark " +
+					orangeLightSelection
+				}
+				onClick={() => setLight("orange")}
+			/>
+			<div
+				className={
+					"green circle rounded-circle border-dark " +
+					greenLightSelection
+				}
+				onClick={() => setLight("green")}
+			/>
 		</div>
 	);
 }
